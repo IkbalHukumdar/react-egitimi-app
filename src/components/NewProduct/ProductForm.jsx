c8d029b1cd2f12bf38efd9bb6947efdd6ea42e5f
Ikbal Hukumdar <99831078+IkbalHukumdar@users.noreply.github.com>
2023-05-15 15:26:24 +0300

    input form eklendi duzenlemeler yapıldı

---
+++ src/components/NewProduct/ProductForm.jsx
@@ -0,0 +1,21 @@
+import "./ProductForm.css";
+const ProductForm = () => {
+  return (
+    <form className="product-form-input">
+      <div className="" product-form-input>
+        <label>Ürün Adı</label>
+        <input type="text" placeholder="Ürün Adı Giriniz:" />
+      </div>
+      <div className="product-form-input">
+        <label>Ürün Fiyatı</label>
+        <input type="number" placeholder="Ürün Fiyatı Giriniz:" />
+      </div>
+      <div className="" product-form-input>
+        <label>Ürün Görseli</label>
+        <input type="text" placeholder="Ürün Görseli Giriniz:" />
+      </div>
+      <button className="product-form-button">Ürün Ekle</button>
+    </form>
+  );
+};
+export default ProductForm;
