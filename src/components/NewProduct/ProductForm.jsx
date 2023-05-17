0e364f9544c58373f37ac71abf64720659804489
Ikbal Hukumdar <99831078+IkbalHukumdar@users.noreply.github.com>
2023-05-17 10:02:37 +0300

    state update işleminde previous state kullanımı

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -2,43 +2,53 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
-  //const [productName, setProductName] = useState("");
-  //const [productPrice, setProductPrice] = useState("");
-  // const [imageUrl, setImageUrl] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   /**
-     tek state içinde durumları yazma 
-     */
-  const [productData, setProductData] = useState({
+    tek state içinde durumları yazma 
+    const [productData, setProductData] = useState({
     // obje oluştu
     productName: "",
-    productPrice:"", 
+    productPrice: "",
     imageUrl: "",
-  });
+  });*/
 
   //fiyat tutacak
   const titleChangeHandler = (event) => {
-    ///setProductName(event.target.value);
+    setProductName(event.target.value);
+    /*
     setProductData({
       ...productData,
       productName: event.target.value,
     });
+    //önceki state alıp karşılaştırma yapıyor
+    //değiştirmek istediğim özelliği değiştiriyor
+    setProductData((prevState) => {
+      return { ...prevState, productName: event.target.value };
+    });*/
   };
-
   const priceChangeHandler = (event) => {
-    //setProductPrice(event.target.value);
-    setProductData({
+    setProductPrice(event.target.value);
+    /*  setProductData({
       ...productData,
       productPrice: event.target.value,
     });
+    setProductData((prevState) => {
+      return { ...prevState, productPrice: event.target.value };
+    });*/
   };
   const imageChangeHandler = (event) => {
-    // setImageUrl(event.target.value);
-    setProductData({
+    setImageUrl(event.target.value);
+    /* setProductData({
       ...productData,
       imageUrl: event.target.value,
     });
+    setProductData((prevState) => {
+      return { ...prevState, imageUrl: event.target.value };
+    });*/
   };
-  console.log(productData.productName);
+ 
 
   return (
     <form className="product-form">
